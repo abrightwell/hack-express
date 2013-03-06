a517e28be306312cd4ecef05c78ee7017adb3305
Adam Brightwell <abrightwell@lce.com>
2013-03-06 01:41:20 -0500

    added initial database setup script, including the initial db view documents necessary for the main hack-express database.

---
+++ hack-express/setup/setup.js
@@ -0,0 +1,49 @@
+var config = require('../config');
+var fs = require('fs');
+var path = require('path');
+
+var nano = require('nano')(config.db.url);
+
+// Setup hack-express database.
+
+nano.db.create(config.db.name, function(err, body) {
+  if (!err) {
+    console.log('Database ' + config.db.name + ' created!');
+  } else {
+    console.log('Error: ' + err.reason);
+  }
+});
+
+// Install to hack-express database design documents.
+
+db = nano.use(config.db.name);
+
+design_docs = fs.readdirSync('./design_docs');
+
+design_docs.forEach(function(design_doc) {
+  file = fs.readFileSync(path.join('./design_docs', design_doc)
+  design = JSON.parse(file, 'utf8'));
+  
+  db.insert(design, function(error, response) {
+    if (!error) {
+      console.log('Successfully installed: ' + design_doc);
+    } else {
+      console.log('Error occurred installing: ' + design_doc);
+      console.log('Error: ' + error.reason);
+    }
+  });
+});
+
+// Setup hack-express-sessions database.
+
+nano.db.create('hack-express-session', function(err, body) {
+  if (!err) {
+    console.log('Database ' + 'hack-express-session' + ' created!');
+  } else {
+    console.log('Error: ' + err.reason);
+  }
+});
+
+// Install hack-express-sessions database design documents.
+
+db = nano.use('hack-express-session');

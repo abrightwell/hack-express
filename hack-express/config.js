6b236d00becbffb7a0cdde5eed24b008f728a125
Adam Brightwell <abrightwell@lce.com>
2013-06-21 18:09:39 -0400

    modified configuration to use defaults and options.

--- hack-express/config.js
+++ hack-express/config.js
@@ -15,67 +15,24 @@
  */
 
 /*
- *Authors:  Adam Brightwell, Robert Dunigan
+ * Authors:  Adam Brightwell, Robert Dunigan
  */
 
-var util = require('util')
+var util = require('util');
+var jquery = require('jquery');
+var fs = require('fs');
+
 var config = {};
+var options = {};
+var defaults = JSON.parse(fs.readFileSync('./conf/defaults.json'));
 
-config.cache = {};
-config.db = {};
-config.session = {};
-config.session.db = {};
-config.redis = {};
-config.ssl = {};
-config.page = {};
-config.log = {};
-config.name = {};
-config.input = {};
-
-config.cache.secure = true;
-config.cache.max_age = 31536000000; //miliseconds, 1yr, Static files
-config.cache.min_age = 15; //seconds, 0.25min, Public pages
-
-config.db.host = 'hack-express-db';
-config.db.port = 27017;
-config.db.ssl = true;
-config.db.poolSize = 5;
-config.db.name = 'hack-express';
-config.db.user = 'hack-admin';
-config.db.password = 'secret';
-
-config.session.db.host = 'hack-express-db';
-config.session.db.port = 27017;
-config.session.db.ssl = false;
-config.session.db.name = 'hack-express-sessions';
-config.session.db.revs_limit = '1000';
-config.session.db.username = 'hack-admin';
-config.session.db.password = 'secret';
-
-config.ssl.key = './security/hack-express-dev.key';
-config.ssl.cert = './security/hack-express-dev.crt';
-
-config.page.refreshTime = 60; //seconds, 1min
-
-config.log.color = true;
-config.log.console = true;
-config.log.file = true;
-config.log.filename = './logs/hack-express.log';
-config.log.level = 'debug';
-config.log.max_size = 8192; //bytes
-
-config.name.max_length = 25;
-
-config.input.types = new Array("username", "password", "confirm", "tokenID");
-
-config.db.connection_string = function() {
-	var ssl = config.session.db.ssl ? 's' : '';
-	return util.format('http%s://%s:%s@%s:%d',
-		ssl,
-		config.db.user,
-		config.db.password,
-		config.db.host,
-		config.db.port);
+if (fs.existsSync('./options.json')) {
+	options = JSON.parse(fs.readFileSync('./conf/options.json'));
+	config = jquery.extend(true, {}, defaults, options);
+} else {
+	config = defaults;
 }
+
+console.log(config);
 
 module.exports = config;

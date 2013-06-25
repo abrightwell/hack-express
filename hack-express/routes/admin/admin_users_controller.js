6045a1a8d749eded4d0147d9dfc0ee47a1106ccd
Adam Brightwell <abrightwell@lce.com>
2013-06-24 21:33:14 -0400

    added index, edit and update methods to the admin_users_controller.

--- hack-express/routes/admin/admin_users_controller.js
+++ hack-express/routes/admin/admin_users_controller.js
@@ -15,13 +15,13 @@
  */
 
 var database = require('../../database').connection;
-var User = require('../../model/team')(database);
+var User = require('../../model/user')(database);
 var log = require('../../log');
 var logger = log.getLogger();
 
 // GET /admin/users
 exports.index = function(req, res) {
-	User.find(function(err, users) {
+	User.find({$query: {}, $orderby: { username: 1 }}, function(err, users) {
 		if (err) {
 			logger.log('error', err);
 		} else {
@@ -47,12 +47,29 @@ exports.show = function(req, res) {
 
 // GET /admin/users/:id/edit
 exports.edit = function(req, res) {
+	var id = req.params.id;
 
+	User.findById(id, function(err, user) {
+		if (err) {
+			logger.log('error', err.reason);
+		} else {
+			res.render('admin/users/edit', {'user': user});
+		}
+	});
 };
 
 // PUT /admin/users/:id
 exports.update = function(req, res) {
+	var id = req.params.id;
+	var user = req.body;
 
+	User.findByIdAndUpdate(id, {$set: user}, function(err, result) {
+		if (err) {
+			logger.log('error', err);
+		} else {
+			res.redirect('/admin/users');
+		}
+	});
 };
 
 // DELETE /admin/users/:id
